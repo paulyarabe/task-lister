7efd39d091556aeb4aceb381930933f6eda2e243
github username <github email address>
2017-08-14 19:41:09 -0400

    can make a list object

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import List from './components/List'
 
+let id = 0
+
 class App extends Component {
 
   constructor(){
     super()
 
     this.state = {
-      Lists: []
+      lists: []
     }
   }
 
-
+  createList = (name) => {
+    let listObject = {
+      id: id++,
+      name: name,
+      tasks: [],
+    }
+    this.setState({
+      lists: [...this.state.lists, listObject]
+    })
+  }
 
   render() {
+    console.log(this.state.lists)
     return (
       <div>
-        <List />
+        <List createListObject={this.createList}/>
       </div>
     );
   }
--- src/components/List.js
+++ src/components/List.js
@@ -2,10 +2,33 @@ import React from 'react'
 
 
 class List extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      listNameInput: ''
+    }
+  }
+
+  handleChange = (e) => {
+    let listNameInput = e.target.value
+    this.setState ({
+      listNameInput: listNameInput
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.createListObject(this.state.listNameInput)
+  }
+
   render() {
     return (
       <div>
-        hi from List
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.listName} onChange={this.handleChange} />
+          <button>Add</button>
+        </form>
       </div>
     );
   }
